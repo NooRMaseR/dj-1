67c796d5290e05549a9ecc21b95ac8c3603df254
NooR MaseR <127132401+NooRMaseR@users.noreply.github.com>
2024-09-11 01:38:17 +0300

    test 2

--- dj_test/settings.py
+++ dj_test/settings.py
@@ -130,6 +130,6 @@ MEDIA_ROOT = BASE_DIR / "mediafiles/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE = True
-# SECURE_HSTS_SECONDS = 10
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 10
